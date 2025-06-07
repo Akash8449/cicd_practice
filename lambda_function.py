24ab457789606ff6d70a3d75dda34d87c4f82c75
Akash8449 <akashgujjar8449@gmail.com>
2025-06-07 18:01:53 +0530

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def lambda_handler(event, context):
+    df = pd.DataFrame({"id": [1, 2, 3, 4, 5, 6],
+                       "raw_grade": ["a", "b", "b", "a", "a", "e"]})
+    print(df.to_string())
+    return {
+        "statusCode": 200,
+        "body": df.to_json()
+    }
+lambda_handler({"key": "value"}, None)  # Example call to the handler
