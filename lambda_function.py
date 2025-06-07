b6a299d7e015decc91e0f968193d0b63e6d83a5a
Akash8449 <akashgujjar8449@gmail.com>
2025-06-07 18:34:43 +0530

    add print in lambda

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,9 @@ import pandas as pd
 def lambda_handler(event, context):
     df = pd.DataFrame({"id": [1, 2, 3, 4, 5, 6],
                        "raw_grade": ["a", "b", "b", "a", "a", "e"]})
+    print("Original DataFrame:")
     print(df.to_string())
+    
     return {
         "statusCode": 200,
         "body": df.to_json()
